54630d4330e161d86acceaa156a8d130a0b58148
agkj <alger.goh99@gmail.com>
2023-03-17 11:42:04 +0800

    Improved registration function - merged from login_interface

--- app/src/main/java/com/example/sc2006_project/AccountRegister.java
+++ app/src/main/java/com/example/sc2006_project/AccountRegister.java
@@ -1,4 +1,4 @@
-package com.example.sc2006_project.boundary;
+package com.example.sc2006_project;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.sc2006_project.MainActivity;
-import com.example.sc2006_project.R;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
