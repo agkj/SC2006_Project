634b26e6398e085dbafd4a3c5b996cc08ffba747
Seo Hyeon <shc.leinad@gmail.com>
2023-04-06 11:40:26 +0800

    Fixed fail cases

--- app/src/main/java/com/example/sc2006_project/boundary/ReservationActivity.java
+++ app/src/main/java/com/example/sc2006_project/boundary/ReservationActivity.java
@@ -1,6 +1,7 @@
 package com.example.sc2006_project.boundary;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +17,12 @@ import com.example.sc2006_project.R;
 import com.example.sc2006_project.entity.Reservation;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -35,13 +40,7 @@ public class ReservationActivity extends AppCompatActivity {
     private RadioGroup timeModeSelector;
     private RadioButton startTimeButton;
     private RadioButton endTimeButton;
-    private int startHour;
-    private int startMinute;
-    private int endHour;
-    private int endMinute;
-
-
-
+    private int startHour, startMinute, endHour, endMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,23 +49,11 @@ public class ReservationActivity extends AppCompatActivity {
 
         FirebaseApp.initializeApp(getApplicationContext());
         timePicker = findViewById(R.id.time_picker);
+        timePicker.setIs24HourView(true);
         timeModeSelector = findViewById(R.id.time_mode_selector);
         startTimeButton = findViewById(R.id.start_time_button);
         endTimeButton = findViewById(R.id.end_time_button);
         Button saveButton = findViewById(R.id.make_reservation);
-
-        // Set OnCheckedChangeListener for the RadioGroup
-        timeModeSelector.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // Update the TimePicker mode based on the selected RadioButton
-                if (checkedId == R.id.start_time_button) {
-                    timePicker.setIs24HourView(true);
-                } else if (checkedId == R.id.end_time_button) {
-                    timePicker.setIs24HourView(false);
-                }
-            }
-        });
 
         RadioButton startTimeButton = findViewById(R.id.start_time_button);
         TextView startTimeTextView = findViewById(R.id.start_time_text_view);
@@ -79,12 +66,12 @@ public class ReservationActivity extends AppCompatActivity {
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
                 if (startTimeButton.isChecked()) {
                     getTimeString(hourOfDay, minute, startTimeTextView);
-                    int startHour = timePicker.getHour();
-                    int startMinute = timePicker.getMinute();
+                    startHour = timePicker.getHour();
+                    startMinute = timePicker.getMinute();
                 } else if (endTimeButton.isChecked()) {
                     getTimeString(hourOfDay, minute, endTimeTextView);
-                    int endHour = timePicker.getHour();
-                    int endMinute = timePicker.getMinute();
+                    endHour = timePicker.getHour();
+                    endMinute = timePicker.getMinute();
                 }
             }
         });
@@ -95,40 +82,64 @@ public class ReservationActivity extends AppCompatActivity {
         DocumentReference documentReference = fStore.collection("users").document(userID);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://parkersc2006-default-rtdb.asia-southeast1.firebasedatabase.app");
-        DatabaseReference myRef = database.getReference("reservations");
+        DatabaseReference reservationsRef = database.getReference("reservations");
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startTimeTextView.getText().toString().equals("Selected Start Time") || endTimeTextView.getText().toString().equals("Selected End Time")) {
-                    Toast.makeText(ReservationActivity.this, "Please select both start and end time", Toast.LENGTH_SHORT).show();
-                    return; // Exit the method to prevent saving invalid reservation data
-                }
-                // Get the selected start and end times from the TextView elements and the user ID from firestore; parking lot passed from previous activity
-                String startTime = startTimeTextView.getText().toString();
-                String endTime = endTimeTextView.getText().toString();
-                String userID = documentReference.getId();
+                long currentTimeMillis = System.currentTimeMillis();
+                Query ongoingReservationQuery = reservationsRef.orderByChild("startTimeMillis").endAt(currentTimeMillis);
 
-                // Check if the end time is after the start time
-                if (endHour < startHour || (endHour == startHour && endMinute <= startMinute)) {
-                    // End time is earlier than start time, so display an error message and return
-                    Toast.makeText(ReservationActivity.this, "End time must be after start time", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                //parking lot variable needs to be passed on from previous activity, which is view map
-                String parkingLot = "parkingLot";
+                ongoingReservationQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            // if ongoing reservation exists
+                            Toast.makeText(ReservationActivity.this, "There is an ongoing reservation", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else {
+                            if (startTimeTextView.getText().toString().equals("Selected Start Time") || endTimeTextView.getText().toString().equals("Selected End Time")) {
+                                Toast.makeText(ReservationActivity.this, "Please select both start and end time", Toast.LENGTH_SHORT).show();
+                                return; // Exit the method to prevent saving invalid reservation data
+                            }
+                            // Get the selected start and end times from the TextView elements and the user ID from firestore; parking lot passed from previous activity
+                            String startTime = startTimeTextView.getText().toString();
+                            String endTime = endTimeTextView.getText().toString();
+                            String userID = documentReference.getId();
 
-                // Create a Reservation object to store the start and end times
-                Reservation reservation = new Reservation(startTime, endTime, userID, parkingLot);
+                            // Check if the end time is after the start time
+                            if (endHour < startHour || (endHour == startHour && endMinute <= startMinute)) {
+                                // End time is earlier than start time, so display an error message and return
+                                Toast.makeText(ReservationActivity.this, "End time must be after start time", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            //parking lot variable needs to be passed on from previous activity, which is view map
+                            String parkingLot = "parkingLot";
 
-                // Write the Reservation object to the database
-                myRef.push().setValue(reservation);
+                            // Create a Reservation object to store the start and end times
+                            Reservation reservation = new Reservation(startTime, endTime, userID, parkingLot);
 
-                // Show a Toast message to indicate that the reservation has been saved
-                Toast.makeText(ReservationActivity.this, "Reservation saved successfully!", Toast.LENGTH_SHORT).show();
+                            // Write the Reservation object to the database
+                            reservationsRef.push().setValue(reservation);
+
+                            // Show a Toast message to indicate that the reservation has been saved
+                            Toast.makeText(ReservationActivity.this, "Reservation saved successfully!", Toast.LENGTH_SHORT).show();
+
+                            //opens navigation page
+                            //Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                            //startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Handle the error
+                    }
+                });
             }
         });
     }
+
 
     private void getTimeString(int hourOfDay, int minute, TextView textView) {
         Calendar calendar = Calendar.getInstance();
